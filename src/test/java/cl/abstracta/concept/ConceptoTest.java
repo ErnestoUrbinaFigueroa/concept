e8f49207227c045af49b5c1dc51942432e4a5ded
Ernesto Urbina F <ernesto.urbina.f@gmail.com>
2019-08-26 15:41:59 -0300

    Agrego el import necesario para firefox.

--- src/test/java/cl/abstracta/concept/ConceptoTest.java
+++ src/test/java/cl/abstracta/concept/ConceptoTest.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.ITestResult;
